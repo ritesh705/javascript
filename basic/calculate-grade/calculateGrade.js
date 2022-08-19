dbc3b99b71faef8e133224d88669c622b7436e81
Ritesh Kumar <ritesh705@gmail.com>
2022-08-19 23:34:11 +0530

    update

--- basic/calculate-grade/calculateGrade.js
+++ basic/calculate-grade/calculateGrade.js
@@ -27,3 +27,9 @@ let grade = function ()
 	// document.write(result); 
 	document.getElementById("display").innerHTML = result;
 }
+
+function sum(){
+	let num1 = parseInt(document.getElementById("num1").value);
+	let num2 = parseInt(document.getElementById("num2").value);
+	console.log(num1+num2);
+}
